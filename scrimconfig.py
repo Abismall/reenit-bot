965f39ca6b7149bfa558b62a870c8a65651e8f11
Abismall <abismayn@gmail.com>
2022-04-05 12:09:20 +0300

    remove server_locations

--- scrimconfig.py
+++ scrimconfig.py
@@ -1,13 +1,1 @@
-server_locations = {
-    "helsinki": ["61bb3f88c6fd3885089a9d9d"
-
-                 ],
-    "stockholm": ["61da24d06e2ab883417b7842"
-                  ],
-    "amsterdam": ["61da24de3428811fce821519"
-                  ],
-    "dusseldorf": ["61da24e36e2ab883417b7843"
-                   ]
-
-}
 map_pool = ["de_dust2", "de_overpass", "de_mirage", "de_cache"]
